d19eb1a460ee21691e9c44e5427b65acd6b38dcd
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2025-09-29 10:42:31 -0300

    feat: Create the first version of the model User

---
+++ backend/src/main/java/com/gabrielrodrigues/encurtador_de_links/enums/RoleEnum.java
@@ -0,0 +1,6 @@
+package com.gabrielrodrigues.encurtador_de_links.enums;
+
+public enum RoleEnum {
+    USER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/gabrielrodrigues/encurtador_de_links/models/User.java
@@ -0,0 +1,88 @@
+package com.gabrielrodrigues.encurtador_de_links.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gabrielrodrigues.encurtador_de_links.enums.RoleEnum;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 50)
+    @Email
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    @NotBlank
+    @JsonIgnore
+    private String password;
+
+    @Column(nullable = false)
+    private RoleEnum role;
+
+    protected User() {}
+
+    public User(Long id, String username, String email, String password, RoleEnum role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public RoleEnum getRole() {
+        return this.role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
